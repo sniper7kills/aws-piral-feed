281a6866e73c83486293ea51be60180b4fb7eee7
Will G <willg@1337hax.org>
2024-03-03 10:24:16 -0500

    Initial commit

---
+++ test/piral-feed.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PiralFeed from '../lib/piral-feed-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/piral-feed-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PiralFeed.PiralFeedStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
